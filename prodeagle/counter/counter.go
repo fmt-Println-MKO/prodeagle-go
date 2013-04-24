f76421e368c7de7125845f8163abd66321e5bb7b
Matthias Koch <mk@umbella.net>
2013-04-24 08:59:39 +0200

    extended counter interface

--- prodeagle/counter/counter.go
+++ prodeagle/counter/counter.go
@@ -2,10 +2,39 @@ package counter
 
 import ()
 
-func Incr(name string) {
+var counters := make([]string,20,50)
 
+func isNewCounter(name string) bool {
+	for c :=range slice {
+		if c == name {
+			return true
+		}
+	}
+	return false
 }
 
-func Commit() {
+func Incr(name string) {
+	Incr(name, 1)
+}
+
+func Incr(name string, int value) {
+	if isNewCounter(name) {
+		append(counter, name)
+	}
+}
+
+type Batch struct {
+	counts := map[string] int
+}
+
+func (b *Batch) Incr(name string) {
+	b.Incr(name,1)
+}
+
+func (b *Batch) Incr(name string, int value) {
+	counts[name]=counts[name]++
+}
+
+func (b *Batch) Commit() {
 
 }
