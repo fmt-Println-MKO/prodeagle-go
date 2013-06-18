ea92025f7c260190d532df38bf252adbe2a6a4b4
Matthias Koch <mk@umbella.net>
2013-06-18 18:11:13 +0200

    fixed docu layout

--- prodeagle/doc.go
+++ prodeagle/doc.go
@@ -2,25 +2,25 @@
 // http://creativecommons.org/licenses/by-sa/3.0/
 
 /*
- prodegale api library for appengine apps 
+prodegale api library for appengine apps 
 
- just count what ever you wanted to get count and watch your app with http://www.prodeagle.com/
- api documentation: http://godoc.org/github.com/fmt-Println-MKO/prodeagle-go/prodeagle
+just count what ever you wanted to get count and watch your app with http://www.prodeagle.com/
+api documentation: http://godoc.org/github.com/fmt-Println-MKO/prodeagle-go/prodeagle
 
- get the api 
+get the api 
 	go get github.com/fmt-Println-MKO/prodeagle-go/prodeagle
 
- import it into your code
- 	import "github.com/fmt-Println-MKO/prodeagle-go/prodeagle"
+import it into your code
+	import "github.com/fmt-Println-MKO/prodeagle-go/prodeagle"
 
- short example how to use:
- add a url for the prodeagle api:
- 	http.HandleFunc("/prodeagle/", prodeagle.Dispatch)
- just count any counter like this:
+short example how to use:
+add a url for the prodeagle api:
+	http.HandleFunc("/prodeagle/", prodeagle.Dispatch)
+just count any counter like this:
 
- c is your appengine.Context
- name is string with name of counter you want to increment by 1
- 	prodeagle.Incr(c, name) 
+c is your appengine.Context
+name is string with name of counter you want to increment by 1
+	prodeagle.Incr(c, name) 
 
 count by more then one:
 delta is an int64 and value is the number the counter should be incremented
@@ -46,6 +46,5 @@ committing batch counter, so that prodeagle harvest can get them
 	b.Commit()
 
 thats all
-
 */
 package prodeagle
