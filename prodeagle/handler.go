05eaf52f1497873c0af1f396dc59e06ffdfbce2f
Matthias Koch <mk@umbella.net>
2013-06-18 17:48:36 +0200

    added license added more documentation

--- prodeagle/handler.go
+++ prodeagle/handler.go
@@ -1,8 +1,14 @@
 /*
- prodegale library for appengine apps 
+ prodegale api library for appengine apps 
 
- just count what ever you wanted to get count and watch your app with prodegle
- see documentation on http://godoc.org/github.com/fmt-Println-MKO/prodeagle-go/prodeagle
+ just count what ever you wanted to get count and watch your app with http://www.prodeagle.com/
+ api documentation: http://godoc.org/github.com/fmt-Println-MKO/prodeagle-go/prodeagle
+
+ get the api 
+	go get github.com/fmt-Println-MKO/prodeagle-go/prodeagle
+
+ import it into your code
+ 	import "github.com/fmt-Println-MKO/prodeagle-go/prodeagle"
 
  short example how to use:
  add a url for the prodeagle api:
@@ -10,12 +16,38 @@
  just count any counter like this:
 
  c is your appengine.Context
- name is the name of counter you want to inceremt by 1
+ name is string with name of counter you want to increment by 1
  	prodeagle.Incr(c, name) 
 
- read the full example on 
- https://github.com/fmt-Println-MKO/prodeagle-go
+count by more then one:
+delta is an int64 and value is the number the counter should be incremented
+	prodeagle.IncrDelta(c, name, delta) 
+
+counting in batchmode
+batchmode means your counter stats are not imedatly written, 
+they will just be written when you commit them. in between this counters are not included during harvest from prodeagle
+
+creating a new Batch Counter
+	var b *prodeagle.Batch
+	if nil == b {
+		b = prodeagle.NewBatch(c)
+	}
+
+incrementing a counter by 1 in batch
+	b.Incr(name)
+
+incrementing a counter by given value in batch
+	b.IncrDelta(name,delta)
+
+committing batch counter, so that prodeagle harvest can get them
+	b.Commit()
+
+thats all
+
 */
+
+// Licensed: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+// http://creativecommons.org/licenses/by-sa/3.0/
 package prodeagle
 
 import (
--- prodeagletest.go
+++ prodeagletest.go
@@ -1,3 +1,5 @@
+// Licensed: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+// http://creativecommons.org/licenses/by-sa/3.0/
 package main
 
 import (
